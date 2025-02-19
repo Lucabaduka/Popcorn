f41d240cb7af7b4c4fb2012e384798a6bbaf8550
Lucabaduka <luca@calref.ca>
2025-02-19 14:39:11 -0800

    Adding logos and nav links

--- Sources/Templates/Pages/main.php
+++ Sources/Templates/Pages/main.php
@@ -15,11 +15,11 @@
 */
 
 // Needs a case statement
-$filter = "ends ASC";
+$sort = "ends ASC";
 
 // Load all current issues and committ them to category arrays
 $all_issues = array("admin" => array(), "conflict" => array(), "economics" => array(), "spots" => array(), "sapphire" => array());
-$query =  "SELECT * FROM topics WHERE result < 1 ORDER BY $filter;";
+$query =  "SELECT * FROM topics WHERE result < 1 ORDER BY $sort;";
 foreach ($pdo->query($query) as $issue) {
   switch ($issue["cat"]) {
     case "admin":
@@ -66,13 +66,29 @@ foreach ($pdo->query($query) as $issue) {
 
   <div class="container mclear py-4">
 
+
+
+
     <div class="notification">
       <div class="tags mb-0 has-addons is-pulled-right">
-        <span class="tag px-2 is-dark is-size-7"><svg style="width:24px; height:auto; filter: invert();" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>PHP</title><path d="M7.01 10.207h-.944l-.515 2.648h.838c.556 0 .97-.105 1.242-.314.272-.21.455-.559.55-1.049.092-.47.05-.802-.124-.995-.175-.193-.523-.29-1.047-.29zM12 5.688C5.373 5.688 0 8.514 0 12s5.373 6.313 12 6.313S24 15.486 24 12c0-3.486-5.373-6.312-12-6.312zm-3.26 7.451c-.261.25-.575.438-.917.551-.336.108-.765.164-1.285.164H5.357l-.327 1.681H3.652l1.23-6.326h2.65c.797 0 1.378.209 1.744.628.366.418.476 1.002.33 1.752a2.836 2.836 0 0 1-.305.847c-.143.255-.33.49-.561.703zm4.024.715l.543-2.799c.063-.318.039-.536-.068-.651-.107-.116-.336-.174-.687-.174H11.46l-.704 3.625H9.388l1.23-6.327h1.367l-.327 1.682h1.218c.767 0 1.295.134 1.586.401s.378.7.263 1.299l-.572 2.944h-1.389zm7.597-2.265a2.782 2.782 0 0 1-.305.847c-.143.255-.33.49-.561.703a2.44 2.44 0 0 1-.917.551c-.336.108-.765.164-1.286.164h-1.18l-.327 1.682h-1.378l1.23-6.326h2.649c.797 0 1.378.209 1.744.628.366.417.477 1.001.331 1.751zM17.766 10.207h-.943l-.516 2.648h.838c.557 0 .971-.105 1.242-.314.272-.21.455-.559.551-1.049.092-.47.049-.802-.125-.995s-.524-.29-1.047-.29z"/></svg></span>
-        <span class="tag px-2 is-info is-size-7">v<?=$version?></span>
+        <span class="tag px-2 is-dark is-size-7">Version</span>
+        <span class="tag px-2 is-success is-size-7"><?=$version?></span>
       </div>
-      <h1 class="title">Popcorn</h1>
-      <h2 class="subtitle is-6">Lofi Bets to Chill and Watch the World Burn to</h2>
+
+      <div class="media">
+        <figure class="image mr-2 is-64x64">
+          <a href="/"><img height="64" width="64" src="/Static/Assets/popcorn_logo.svg" alt="logo" aria-label="Return to splash page"></a>
+        </figure>
+        <div class="media-content mb-3">
+          <h1 class="title">
+            Popcorn
+          </h1>
+          <h2 class="subtitle is-6">
+            Lofi Bets to Chill and Watch the World Burn to
+          </h2>
+        </div>
+      </div>
+
       <div class="content is-primary">
         <p>
           Explore the bets below and wager what you think will happen in this unfathomably unstable world.
@@ -80,7 +96,7 @@ foreach ($pdo->query($query) as $issue) {
       </div>
 
       <!-- Current Operator Balance and Max -->
-      <article class="message is-info">
+      <article class="message is-success">
         <div class="message-body py-4">
           <div class="is-flex center-align" style="flex-wrap: wrap;">
             <p class="mx-2"><strong>Balance:</strong> <code><?=number_format($op["bal"])?></code></p>
@@ -199,7 +215,7 @@ foreach ($pdo->query($query) as $issue) {
                     if ($eligible) {
                       echo "<span class=\"tag slap is-warning\">Bet Available</span>";
                     } else {
-                      echo "<span class=\"tag slap is-success\">Bet in Place</span>";
+                      echo "<span class=\"tag slap is-link\">Bet in Place</span>";
                     }
 
                   ?>
