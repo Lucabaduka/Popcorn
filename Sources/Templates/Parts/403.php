19a92d09136b316f2d4dd68d1c49f74e0d732bd2
Lucabaduka <luca@calref.ca>
2025-02-21 23:15:06 -0800

    Minor tweaks

--- Sources/Templates/Parts/403.php
+++ Sources/Templates/Parts/403.php
@@ -2,8 +2,8 @@
 
 $title    = "403: Access Denied";
 $subtitle = "You do not have the authority to be here.";
-$text     = "You have just tried to do something that requires admin permissions, however there are in fact checks to verify
-             that you actually have the those permissions.<br><br>(You do not, by the way)";
+$text     = "You have just tried to do something that requires admin permissions, however there are actually checks to verify
+             that you really do have those permissions.<br><br>(You do not, by the way)";
 
 include $pages . "page.php";
 
--- Sources/Templates/Parts/404.php
+++ Sources/Templates/Parts/404.php
@@ -2,7 +2,7 @@
 
 $title    = "404: Not Found";
 $subtitle = "There's nothing here";
-$text     = "The page you are looking for does not exist within Popcorn. If there is supposed to be a file here, it must
+$text     = "The page you are looking for does not exist within Popcorn. If there's supposed to be a file here, it must
              have been removed or deleted. If there <i>used</i> to be something here that disappeared suddenly, it may
              be worth reporting to an Admin.";
 
